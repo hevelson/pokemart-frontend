9ebd2c857d5b8ea37811bba14b4b46796af2c3e7
Hevelson <hevelson@gmail.com>
2020-08-26 23:11:25 -0300

    feito(pagamentos): criado componente para listagem de pagamentos

---
+++ src/components/Payments/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Payments = () => {
+  return(
+    <section className="section-payments">
+      <div className="container">
+        <div className="accepted-payments">
+          <h2>Formas de pagamento</h2>
+          <ul>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+            <li><img src="https://dummyimage.com/60x30/bbb/000" alt="60x30" /></li>
+          </ul>
+        </div>
+        <div className="security-certificates">
+          <h2>Certificados de segurança</h2>
+          <ul>
+              <li><img src="https://dummyimage.com/95x90/bbb/000" alt="95x90"/></li>
+              <li><img src="https://dummyimage.com/95x90/bbb/000" alt="95x90"/></li>
+              <li><img src="https://dummyimage.com/95x90/bbb/000" alt="95x90"/></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Payments;
