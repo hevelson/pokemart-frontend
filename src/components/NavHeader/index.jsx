8fcd974322610188fd29f9fec2c2225d67bb0d21
Hevelson <hevelson@gmail.com>
2020-08-29 00:20:58 -0300

    feito(cabeçalho): ajustado largura e adicionado logo

--- src/components/NavHeader/index.jsx
+++ src/components/NavHeader/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { FaUser, FaShoppingCart } from 'react-icons/fa';
 
+import Logo from '../../assets/images/pokemart-logo.png';
+
 const NavHeader = () => {
 
   return (  
     <header className="nav-header">
-      <h1>Pokemart</h1>
-      <div className="right-elements">
-        <button className="btn-user"><FaUser /></button>
-        <button className="btn-cart"><FaShoppingCart /></button>
+      <div className="container">
+        <h1>
+          <img src={Logo} alt="Pokemart" />
+        </h1>
+        <div className="right-elements">
+          <button className="btn-user"><FaUser /></button>
+          <button className="btn-cart"><FaShoppingCart /></button>
+        </div>
       </div>
     </header>
   );
