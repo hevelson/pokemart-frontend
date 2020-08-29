866c3b93135fe95737cbe39d77299ff9616d6d7d
Hevelson <hevelson@gmail.com>
2020-08-28 23:55:36 -0300

    feito(serviços): adicionado serviço de detalhes do produto

--- src/services/products.js
+++ src/services/products.js
@@ -18,4 +18,20 @@ export const getProducts = (page) => {
   });
 };
 
+export const getProduct = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await get(`/api/v1/product/${id}`);
+
+      if (response.data.error || response.data.errors) {
+        throw new Error(response.data);
+      }
+
+      resolve(response.data);
+    } catch (error) {
+      reject(getErrorMessage(error));
+    }
+  });
+};
+
 export default { getProducts };
