0de030bacc30901098e6ff3c1f89f06d833d8303
Hevelson <hevelson@gmail.com>
2020-09-06 11:04:28 -0300

    feito(estilo): ajustado estilo de página 404

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const HomePage = () => {
 
 	return(
-		<main className="home-page">
-            <h1>404 - Página não encontrada</h1>
+		<main className="not-found-page">
+			<h1>404 - Página não encontrada</h1>
 		</main>
 	);
 }
